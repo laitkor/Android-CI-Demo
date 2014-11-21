3a168e04e8a50f2cd74e8f66f47169f4d0891a03
Sergii Zhuk <sergey.public@gmail.com>
2014-11-22 01:19:28 +0200

    gradle config update, target api-21 again

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,10 +6,9 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t extra &&
-  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
+  echo y | android update sdk -u -a -t build-tools-21.1.1
   touch $DEPS
 fi
